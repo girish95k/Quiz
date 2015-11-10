e7a8d243f9c021b30039e355bce8ed84ccee9d3e
Girish <girish95k@gmail.com>
2015-11-10 21:48:24 +0530

    graph1

--- app/src/main/java/com/pes/gkl/gate/SplashActivity.java
+++ app/src/main/java/com/pes/gkl/gate/SplashActivity.java
@@ -1,13 +1,24 @@
 package com.pes.gkl.gate;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.RadarChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.RadarData;
+import com.github.mikephil.charting.data.RadarDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
 public class SplashActivity extends ActionBarActivity {
     /** Duration of wait **/
     private final int SPLASH_DISPLAY_LENGTH = 2000;
+    RadarChart mChart;
+    Typeface tf;
 
     /** Called when the activity is first created. */
     @Override
@@ -17,14 +28,72 @@ public class SplashActivity extends ActionBarActivity {
 
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
+        
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
                 Intent mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
                 SplashActivity.this.startActivity(mainIntent);
                 SplashActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
+
+
+        mChart = (RadarChart)findViewById(R.id.chart);
+        tf = Typeface.createFromAsset(getAssets(), "OpenSans-Regular.ttf");
+
+        setData();
+    }
+
+    private String[] mParties = new String[] {
+            "Data Structures", "Algorithms", "COA", "Networks"
+    };
+
+    public void setData() {
+
+        float mult = 150;
+        int cnt = 4;
+
+        ArrayList<Entry> yVals1 = new ArrayList<Entry>();
+        ArrayList<Entry> yVals2 = new ArrayList<Entry>();
+
+        // IMPORTANT: In a PieChart, no values (Entry) should have the same
+        // xIndex (even if from different DataSets), since no values can be
+        // drawn above each other.
+        for (int i = 0; i < cnt; i++) {
+            yVals1.add(new Entry((float) (Math.random() * mult) + mult / 2, i));
+        }
+
+        for (int i = 0; i < cnt; i++) {
+            yVals2.add(new Entry((float) (Math.random() * mult) + mult / 2, i));
+        }
+
+        ArrayList<String> xVals = new ArrayList<String>();
+
+        for (int i = 0; i < cnt; i++)
+            xVals.add(mParties[i % mParties.length]);
+
+        RadarDataSet set1 = new RadarDataSet(yVals1, "You");
+        set1.setColor(ColorTemplate.VORDIPLOM_COLORS[0]);
+        set1.setDrawFilled(true);
+        set1.setLineWidth(2f);
+
+        RadarDataSet set2 = new RadarDataSet(yVals2, "World Average");
+        set2.setColor(ColorTemplate.VORDIPLOM_COLORS[4]);
+        set2.setDrawFilled(true);
+        set2.setLineWidth(2f);
+
+        ArrayList<RadarDataSet> sets = new ArrayList<RadarDataSet>();
+        sets.add(set1);
+        sets.add(set2);
+
+        RadarData data = new RadarData(xVals, sets);
+        data.setValueTypeface(tf);
+        data.setValueTextSize(8f);
+        data.setDrawValues(false);
+
+        mChart.setData(data);
+
+        mChart.invalidate();
     }
 }
